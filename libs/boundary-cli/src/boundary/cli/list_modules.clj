b67e6e79ff73811a40e7a429f2c7dd554084a27b
thijscreemers <thijs.creemers@gmail.com>
2026-04-30 06:21:01 +0200

    feat: implement boundary list modules
    Add list-modules namespace with print-table and print-json functions,
    and a test suite covering human-readable table output and JSON format.

---
+++ libs/boundary-cli/src/boundary/cli/list_modules.clj
@@ -0,0 +1,44 @@
+(ns boundary.cli.list-modules
+  (:require [boundary.cli.catalogue :as cat]
+            [cheshire.core :as json]
+            [clojure.string :as str]))
+
+(defn- pad [s width]
+  (let [s (str s)]
+    (if (>= (count s) width)
+      (subs s 0 width)
+      (str s (apply str (repeat (- width (count s)) " "))))))
+
+(defn print-table []
+  (let [modules (cat/optional-modules)
+        fmt     "  %-12s  %-50s  %s"]
+    (println)
+    (println (format fmt "Module" "Description" "Command"))
+    (println (format fmt (apply str (repeat 12 "-"))
+                     (apply str (repeat 50 "-"))
+                     (apply str (repeat 28 "-"))))
+    (doseq [{:keys [name description add-command]} modules]
+      (println (format fmt (pad name 12) (pad description 50) add-command)))
+    (println)))
+
+(defn print-json []
+  (let [catalogue (cat/load-catalogue)
+        modules   (map (fn [m]
+                         {:name             (:name m)
+                          :description      (:description m)
+                          :clojars          (str (:clojars m))
+                          :version          (:version m)
+                          :category         (name (:category m))
+                          :add-command      (:add-command m)
+                          :docs-url         (:docs-url m)})
+                       (:modules catalogue))]
+    (println (json/generate-string
+              {:cli-version       (:cli-version catalogue)
+               :catalogue-version (:catalogue-version catalogue)
+               :modules           modules}
+              {:pretty true}))))
+
+(defn -main [args]
+  (if (some #(= "--json" %) args)
+    (print-json)
+    (print-table)))
